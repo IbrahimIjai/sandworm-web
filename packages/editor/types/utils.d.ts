db28b0adf548cd8584b0fb0c5c5e30d4ca36b36c
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-10-11 01:30:11 +0100

    chore: deleted d.ts files in packages/editor/types folder

--- packages/editor/types/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import * as z from 'zod';
-export declare function exhaustiveCheck(_param: never): void;
-export declare const uuidSchema: z.ZodEffects<z.ZodString, string, string>;
-//# sourceMappingURL=utils.d.ts.map
